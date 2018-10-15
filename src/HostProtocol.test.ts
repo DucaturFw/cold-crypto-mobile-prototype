76e6520a68992670bdf64fec3def3983dc73609b
zlumer <zlumer@gmail.com>
2018-10-15 21:11:08 +0300

    host protocol parser

---
+++ src/HostProtocol.test.ts
@@ -0,0 +1,65 @@
+import { arrayToObj, objToArray, parseHostCommand, allToObj, allToArray, parseHostCommandToArray } from "./HostProtocol";
+
+describe('host protocol tests', () =>
+{
+	it('should parse normal commands', () =>
+	{
+		expect(parseHostCommand('test|5|[1,2,3]'))
+			.toEqual({ method: "test", id: "5", args: [1,2,3] })
+		expect(parseHostCommand('test2||["hi"]'))
+			.toEqual({ method: "test2", id: '', args: ["hi"] })
+		expect(parseHostCommand('|5|[1,{"hi":"hello"},3]'))
+			.toEqual({ method: "", id: "5", args: [1,{hi: "hello"},3] })
+		expect(parseHostCommand('||[]'))
+			.toEqual({ method: "", id: "", args: [] })
+		expect(parseHostCommand('test2||["h|i"]'))
+			.toEqual({ method: "test2", id: '', args: ["h|i"] })
+
+		expect(parseHostCommand('test|5|{"foo":"bar","test":"test"}'))
+			.toEqual({ method: "test", id: "5", args: {foo:"bar",test:"test"} })
+		expect(parseHostCommand('test2||{"foo":"ba|r"}'))
+			.toEqual({ method: "test2", id: '', args: {foo:"ba|r"} })
+		expect(parseHostCommand('|5|{"hi":"hello"}'))
+			.toEqual({ method: "", id: "5", args: {hi: "hello"} })
+		expect(parseHostCommand('||{}'))
+			.toEqual({ method: "", id: "", args: {} })
+	})
+	it('should parse array commands', () =>
+	{
+		expect(parseHostCommandToArray('test|5|[1,2,3]', ["foo", "bar", "baz"]))
+			.toEqual({ method: "test", id: "5", args: [1,2,3] })
+		expect(parseHostCommandToArray('test|5|{"foo":1,"bar":2,"baz":3}', ["foo", "bar", "baz"]))
+			.toEqual({ method: "test", id: "5", args: [1,2,3] })
+	})
+	it('should parse object commands', () =>
+	{
+		expect(parseHostCommandToArray('test|5|[1,2,3]', ["foo", "bar", "baz"]))
+			.toEqual({ method: "test", id: "5", args: {foo:1, bar:2, baz:3} })
+		expect(parseHostCommandToArray('test|5|{"foo":1,"bar":2,"baz":3}', ["foo", "bar", "baz"]))
+			.toEqual({ method: "test", id: "5", args: {foo:1, bar:2, baz:3} })
+	})
+	it('should convert arrays to objects', () =>
+	{
+		let obj: {foo: number, bar: number, baz: number, test: string} = arrayToObj([1, 2, 3, 'test'], ['foo', 'bar', 'baz', 'test'])
+		expect(obj).toEqual({ foo: 1, bar: 2, baz: 3, test: 'test' })
+	})
+	it('should convert objects to arrays', () =>
+	{
+		let arr = objToArray({ foo: 1, bar: 2, baz: 3, test: "test" }, ["test", "foo", "bar", "baz"])
+		expect(arr).toEqual(["test", 1, 2, 3])
+	})
+	it('should convert anything to objects', () =>
+	{
+		let obj = allToObj<{foo: number, bar: number, baz: number, test: string}>([1, 2, 3, 'test'], ['foo', 'bar', 'baz', 'test'])
+		expect(obj).toEqual({ foo: 1, bar: 2, baz: 3, test: 'test' })
+		obj = allToObj(obj, ['foo', 'bar', 'baz', 'test'])
+		expect(obj).toEqual({ foo: 1, bar: 2, baz: 3, test: 'test' })
+	})
+	it('should convert anything to arrays', () =>
+	{
+		let arr = allToArray({ foo: 1, bar: 2, baz: 3, test: "test" }, ["test", "foo", "bar", "baz"])
+		expect(arr).toEqual(["test", 1, 2, 3])
+		arr = allToArray(arr, ['foo', 'bar', 'baz', 'test'])
+		expect(arr).toEqual(["test", 1, 2, 3])
+	})
+})
---
+++ src/HostProtocol.ts
@@ -0,0 +1,42 @@
+export interface IHostCommand<TArr extends any[], TObj extends {}>
+{
+	method: string
+	id: string | number
+	args: TArr | TObj
+}
+export function parseHostCommand(msg: string): IHostCommand<any[], any>
+{
+	let [method, id, data] = msg.split('|', 3)
+	let args = JSON.parse(data || "[]")
+	return {
+		method,
+		id,
+		args
+	}
+}
+export function parseHostCommandToArray<TArr extends TObj[keyof TObj][], TObj>(msg: string, mapping: (keyof TObj)[]): IHostCommand<TArr, never>
+{
+	let m = parseHostCommand(msg)
+	return { ...m, args: allToArray(m.args, mapping) }
+}
+export function parseHostCommandToObject<TObj>(msg: string, mapping: (keyof TObj)[]): IHostCommand<never, TObj>
+{
+	let m = parseHostCommand(msg)
+	return { ...m, args: allToObj(m.args, mapping) }
+}
+export function arrayToObj<TArr extends any[], TObj>(args: TArr, mapping: (keyof TObj)[]): TObj
+{
+	return args.reduce((acc, cur, idx) => (acc[mapping[idx]] = cur, acc), {})
+}
+export function objToArray<TArr extends TObj[keyof TObj][], TObj extends {}>(args: TObj, mapping: (keyof TObj)[]): TArr
+{
+	return mapping.map(name => args[name]) as TArr
+}
+export function allToObj<TObj>(args: TObj | TObj[keyof TObj][], mapping: (keyof TObj)[]): TObj
+{
+	return Array.isArray(args) ? arrayToObj(args, mapping) : args
+}
+export function allToArray<TArr extends TObj[keyof TObj][], TObj>(args: TObj | TArr, mapping: (keyof TObj)[]): TArr
+{
+	return Array.isArray(args) ? args : objToArray(args, mapping)
+}
