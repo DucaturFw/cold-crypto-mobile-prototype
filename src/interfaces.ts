6a07093633cd4972afaa5fba550c14c48cff9042
zlumer <zlumer@gmail.com>
2018-10-16 17:16:04 +0300

    interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+export type IBlockchainIdent = "eth" | "neo" | "eos" | "btc"
+export interface IWallet
+{
+	blockchain: IBlockchainIdent
+	address: string
+	chainId: number | string
+}
+export interface IEthTransferTxRequest
+{
+	to: string
+	nonce: number
+	gasPrice: string
+	value: string
+	// data: string // hard-coded for value transfers
+	// gasLimit: string // hard-coded for value transfers
+	// from: string // can be determined from wallet
+	// chainId: number // can be determined from wallet
+}
